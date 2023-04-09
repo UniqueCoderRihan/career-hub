ef1f050745c5d41ca7ec84eecde4d127b90ef97e
Rihanul Islam <rihanthedev@gmail.com>
2023-04-09 22:16:02 +0600

    Header done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,18 +5,20 @@ import Nabvar from '../Nabvar/Nabvar';
 const Header = () => {
     return (
 
-        <div>
-            <div>
-                <Nabvar></Nabvar>
-                <div>
-                    <h4>One Step </h4>
-                    <h4>Closer To Your</h4>
-                    <h4><span>Dream Job</span></h4>
-                    <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
-                    <button className='btn btn-liner'>Get Started</button>
+        <div className='mx-5'>
+            <Nabvar></Nabvar>
+            <div className='grid md:grid-cols-2'>
+                <div className='w-50% my-auto'>
+                    <div>
+                        <h4 className='text-5xl font-semibold'>One Step </h4>
+                        <h4 className='text-5xl font-semibold'>Closer To Your</h4>
+                        <h4 className='text-5xl font-semibold'><span className='primaryColour'>Dream Job</span></h4>
+                        <p className='text-2xl my-4'>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+                        <button className='btn btn-liner'>Get Started</button>
+                    </div>
                 </div>
-                <div>
-
+                <div className='w-50% mt-0'>
+                    <img className='mx-auto' src="https://i.ibb.co/Vts4CBc/dsdsd.png" alt="dsdsd" border="0" />
                 </div>
             </div>
         </div>
