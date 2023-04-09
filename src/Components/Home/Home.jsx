f9eee0b65a4b7dd5b53b446da2a10c163851f4d8
Rihanul Islam <rihanthedev@gmail.com>
2023-04-09 23:56:25 +0600

    Header Done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Nabvar from '../Nabvar/Nabvar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../Header/Header';
 
 const Home = () => {
+    const location = useLocation()
     return (
         <div>
             <Nabvar></Nabvar>
+            {
+                location.pathname =='/'?<Header></Header>:``
+            }
             <Outlet></Outlet>
         </div>
     );
