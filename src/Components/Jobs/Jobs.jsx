26d435380d1f39fbbe336120520848c3c75ca899
Rihanul Islam <rihanthedev@gmail.com>
2023-04-10 00:37:59 +0600

    job category 1.0

---
+++ src/Components/Jobs/Jobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Jobs = () => {
+    return (
+        <div>
+            <div className='text-center my-3'>
+                <h3 className=' text-4xl font-semibold'>Job Category List</h3>
+                <p className='mt-2 '>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+        </div>
+    );
+};
+
+export default Jobs;
--- src/Components/Statistic/Statistic.jsx
+++ src/Components/Statistic/Statistic.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
 import Nabvar from '../Nabvar/Nabvar';
+import Jobs from '../Jobs/Jobs';
 
 const Statistic = () => {
     return (
         <div>
-            
+            <Jobs></Jobs>
         </div>
     );
 };
