4e622bc99010eb7ac3cfaee81edb79efded96de9
Rihanul Islam <rihanthedev@gmail.com>
2023-04-10 23:19:21 +0600

    active link added

--- src/Components/Nabvar/Nabvar.jsx
+++ src/Components/Nabvar/Nabvar.jsx
@@ -6,15 +6,23 @@ const Nabvar = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="navbar-start">
-                
+
                 <a className="text-2xl font-semibold">BDJOBS</a>
             </div>
             <div className="navbar-center lg:flex">
                 <ul className="menu menu-horizontal px-1 pages mr-5">
-                    <Link to='/'>Home</Link>
-                    <Link to='/statistic'>Statistics</Link>
-                    <Link to='/applied'>Applied Job</Link>
-                    <Link to='/blogs'>Blogs</Link>
+                    <Link to='/' className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }>Home</Link>
+                    <Link to='/statistic' className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }>Statistics</Link>
+                    <Link to='/applied' className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }>Applied Job</Link>
+                    <Link to='/blogs' className={({ isActive, isPending }) =>
+                        isPending ? "pending" : isActive ? "active" : ""
+                    }>Blogs</Link>
                 </ul>
             </div>
             <div className="navbar-end">
