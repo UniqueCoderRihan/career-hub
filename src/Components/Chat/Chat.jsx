7d54fe30f9939743928c234c60826925d3748c16
Rihanul Islam <rihanthedev@gmail.com>
2023-04-11 01:18:23 +0600

    Chart added

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Line } from 'recharts';
 const Chat = () => {
+    const marks = [
+        { name: 'Assignment 1', marks: 60 },
+        { name: 'Assignment 2', marks: 60 },
+        { name: 'Assignment 3', marks: 58 },
+        { name: 'Assignment 4', marks: 52 },
+        { name: 'Assignment 5', marks: 60 },
+        { name: 'Assignment 6', marks: 59 },
+        { name: 'Assignment 7', marks: 59 },
+    ];
     return (
         <div>
-            <h3>Hi Bodda ami line Chat thek boltece</h3>
+           <AreaChart
+                width={1000}
+                height={300}
+                data={marks}
+                >
+                    <Area dataKey="marks"></Area>
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </AreaChart>
         </div>
     );
 };
